70d01e14813621eb95723e85ce89b8d0ec9a6e1b
Ukabix <54454097+Ukabix@users.noreply.github.com>
2020-01-27 16:03:55 +0100

    27012020

--- section 2 - basics/basics-assignment-1-start/assignment.js
+++
@@ -1,7 +0,0 @@
-let userInput = 213;
-let result;
-result = 18 + userInput;
-result = result * 1.3;
-result = result + 15;
-alert(userInput);
-alert(result);
---
+++ section 2 - basics/basics-assignment-1/assignment.js
@@ -0,0 +1,7 @@
+let userInput = 213;
+let result;
+result = 18 + userInput;
+result = result * 1.3;
+result = result + 15;
+alert(userInput);
+alert(result);
