243de32a1fdc3c207603933b9630b16234ef409b
Ukabix <54454097+Ukabix@users.noreply.github.com>
2020-01-28 23:30:39 +0100

    28012020

--- section 2 - basics/basics-00-current-project/assets/scripts/app.js
+++ section 2 - basics/basics-00-current-project/assets/scripts/app.js
@@ -2,10 +2,56 @@ alert('this works!');
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+
+// This is a comment.
+/*
+This
+is
+a
+cooler
+comment.
+*/
+
+// gets input from input field
+function getUserInput() {
+    return parseInt(usrInput.value);
+}
+
+// generates and writes calculation log
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); //from vendor file
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLog('+', initialResult, enteredNumber);    
+}
+
+function subtract () {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
-
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
