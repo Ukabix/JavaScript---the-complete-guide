7f81df6f2cf4c94f226af08588c380fcfe4778fb
Ukabix <54454097+Ukabix@users.noreply.github.com>
2020-01-28 17:28:43 +0100

    28012020

--- section 2 - basics/basics-08-user-input-function/assets/scripts/app.js
+++ section 2 - basics/basics-08-user-input-function/assets/scripts/app.js
@@ -1,11 +1,9 @@
-alert('this works!');
 const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
 }
 
 addBtn.addEventListener('click', add);
-
