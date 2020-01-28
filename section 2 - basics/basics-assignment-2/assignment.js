29009955f3c0d56f694068a394ddf883da7cf11f
Ukabix <54454097+Ukabix@users.noreply.github.com>
2020-01-28 17:58:25 +0100

    28012020

--- section 2 - basics/basics-assignment-2/assignment.js
+++ section 2 - basics/basics-assignment-2/assignment.js
@@ -1,1 +1,21 @@
 const task3Element = document.getElementById('task-3');
+
+function ahtung() {
+    alert('this is an alert!');
+}
+
+function callName(name) {
+    alert('hello' + name);
+}
+
+function threeStrings(str1, str2, str3) {
+    let newText = `${str1}${str2}${str3}`;
+    return newText;
+}
+
+ahtung();
+callName('łukasz');
+
+task3Element.addEventListener('click', ahtung);
+let newText = threeStrings('cześć', 'czołem', 'kluski pod stołem');
+alert(newText);
